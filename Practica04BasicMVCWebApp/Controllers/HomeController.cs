2fe6fe2e6d870e11065651d215ad93fe67b44bd8
Juan Carlos <juan.gallegos@bachoco.net>
2019-02-16 10:08:54 -0600

    Add project files.

---
+++ Practica04BasicMVCWebApp/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Practica04BasicMVCWebApp.Models;
+
+namespace Practica04BasicMVCWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public string TestPage()
+        {
+            return "Saludo!!";
+        }
+
+        public IActionResult TestPage2() {
+        
+            ViewData["Mensaje"] ="Página que utiliza una vista sin modelo";
+            return View();
+        }
+
+        public IActionResult TestPage3()
+        {
+            Persona juancarlos = new Persona
+            {
+                Nombre = "Juan Carlos",
+                Edad = 20
+            };
+            return View(juancarlos);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
